6adc843bd3b060ea76a5e333ac86fa6918a252f7
kmgyu <lidersy961@gmail.com>
2025-08-11 22:33:07 +0900

    feat: 게시글 CRUD 기능 추가

--- src/main/java/groom/dashboardpractice/crud/DashboardController.java
+++ src/main/java/groom/dashboardpractice/crud/DashboardController.java
@@ -1,15 +1,37 @@
 package groom.dashboardpractice.crud;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class DashboardController {
+    private final DashboardService dashboardService;
+
     @RequestMapping(value = "/post", method = RequestMethod.POST)
     public Post createPost(Post post) {
-        return null;
+        return dashboardService.createPost(post);
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.PATCH)
+    public Boolean updatePost(Post post) {
+        return dashboardService.updatePost(post);
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.GET)
+    public List<Post> getPostList(Post post) {
+        return dashboardService.findPostAll();
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
+    public Post getPost(@PathVariable int id) {
+        return dashboardService.findPostById(id);
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.DELETE)
+    public Boolean deletePost(@PathVariable int id) {
+        return dashboardService.deletePostById(id);
     }
 }
--- src/main/java/groom/dashboardpractice/crud/DashboardService.java
+++ src/main/java/groom/dashboardpractice/crud/DashboardService.java
@@ -3,24 +3,39 @@ package groom.dashboardpractice.crud;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class DashboardService {
     private final DashboardRepository dashboardRepository;
 
     public Post createPost(Post post) {
-        return null;
+        return dashboardRepository.save(post);
     }
 
+    public List<Post> findPostAll() {
+        return dashboardRepository.findAll();
+    }
+
+//    isPresent 검사 없음.
+//    Todo : 조건 분기 추가
     public Post findPostById(int id) {
-        return null;
+        return dashboardRepository.findById(id).get();
     }
 
     public Boolean updatePost(Post post) {
-        return false;
+        if (dashboardRepository.existsById(post.getId())) {
+            dashboardRepository.save(post);
+        }
+        return dashboardRepository.existsById(post.getId());
     }
 
     public Boolean deletePostById(int id) {
+        if (dashboardRepository.existsById(id)) {
+            dashboardRepository.deleteById(id);
+            return true;
+        }
         return false;
     }
 }
