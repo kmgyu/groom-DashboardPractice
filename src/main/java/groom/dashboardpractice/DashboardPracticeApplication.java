f9cd98c4ae3f49fd677ce65b1bb400a594dc04ae
kmgyu <lidersy961@gmail.com>
2025-08-11 20:38:07 +0900

    init commit

---
+++ src/main/java/groom/dashboardpractice/DashboardPracticeApplication.java
@@ -0,0 +1,13 @@
+package groom.dashboardpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/groom/dashboardpractice/DashboardPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package groom.dashboardpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
