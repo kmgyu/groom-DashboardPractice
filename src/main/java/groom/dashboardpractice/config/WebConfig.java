25aaacd6ae256bc036ffbd83812e12b8e686a964
kmgyu <lidersy961@gmail.com>
2025-08-14 13:05:47 +0900

    chore: 사용하지 않는 클래스 제거

--- src/main/java/groom/dashboardpractice/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package groom.dashboardpractice.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
-//    대체 이게 왜 필요하다는거야?
-}
