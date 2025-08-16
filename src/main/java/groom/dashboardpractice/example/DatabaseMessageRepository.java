e0a9729c9d2d802541e032065615acf96b30344a
kmgyu <lidersy961@gmail.com>
2025-08-16 22:44:03 +0900

    Task (#2): @Primary를 사용하여 기본 빈 설정하기

--- src/main/java/groom/dashboardpractice/example/DatabaseMessageRepository.java
+++ src/main/java/groom/dashboardpractice/example/DatabaseMessageRepository.java
@@ -3,6 +3,9 @@ package groom.dashboardpractice.example;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+/**
+ * Primary를 활용한 빈 주입
+ */
 @Component
 @Primary
 public class DatabaseMessageRepository implements MessageRepository {
